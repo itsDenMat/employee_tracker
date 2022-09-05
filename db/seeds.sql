64667f4dc1a095ee22dc215d19e9638d55da2d57
itsDenMat <dennismateo93@gmail.com>
2022-09-04 23:23:12 -0700

    Add seeds.sql

---
+++ db/seeds.sql
@@ -0,0 +1,88 @@
+-- Departments table
+INSERT INTO Departments (name)
+VALUES ("Engineering");
+
+INSERT INTO Departments (name)
+VALUES ("Finance");
+
+INSERT INTO Departments (name)
+VALUES ("Legal");
+
+INSERT INTO Departments (name)
+VALUES ("Sales");
+
+-- Roles table
+INSERT INTO Roles (title, salary, dept_id)
+VALUES ("Accountant", 60000, 2);
+
+INSERT INTO Roles (title, salary, dept_id)
+VALUES ("Account Manager", 65000, 2);
+
+INSERT INTO Roles (title, salary, dept_id)
+VALUES ("Lawyer", 125000, 3);
+
+INSERT INTO Roles (title, salary, dept_id)
+VALUES ("Lead Engineer", 130000, 1);
+
+INSERT INTO Roles (title, salary, dept_id)
+VALUES ("Legal Team Lead", 97000, 3);
+
+INSERT INTO Roles (title, salary, dept_id)
+VALUES ("Sales Lead", 78000, 4;
+
+INSERT INTO Roles (title, salary, dept_id)
+VALUES ("Salesperson", 65000, 4);
+
+INSERT INTO Roles (title, salary, dept_id)
+VALUES ("Software Engineer", 123000, 1);
+
+-- Employees table
+INSERT INTO Employees (first_name, last_name, role_id)
+VALUES ("Casey", "Roberts", 6);
+
+INSERT INTO Employees (first_name, last_name, role_id)
+VALUES ("Christopher", "Sexton", 4);
+
+INSERT INTO Employees (first_name, last_name, role_id)
+VALUES ("Melanie", "Boon", 6);
+
+INSERT INTO Employees (first_name, last_name, role_id)
+VALUES ("Andrew", "Sanders", 2);
+
+INSERT INTO Employees (first_name, last_name, role_id)
+VALUES ("Nancy", "Carlson", 5);
+
+INSERT INTO Employees (first_name, last_name, role_id)
+VALUES ("Edward", "Fernandez", 4);
+
+INSERT INTO Employees (first_name, last_name, role_id)
+VALUES ("Daniel", "Beard", 7);
+
+INSERT INTO Employees (first_name, last_name, role_id)
+VALUES ("Barbara", "Lewis", 2);
+
+INSERT INTO Employees (first_name, last_name, role_id)
+VALUES ("Latoya", "Rollins", 7);
+
+INSERT INTO Employees (first_name, last_name, role_id)
+VALUES ("Amy", "Snow", 1);
+
+INSERT INTO Employees (first_name, last_name, role_id)
+VALUES ("Charles", "Harper", 3);
+
+INSERT INTO Employees (first_name, last_name, role_id)
+VALUES ("Christopher", "Stewart", 1);
+
+INSERT INTO Employees (first_name, last_name, role_id)
+VALUES ("April", "Meadows", 8);
+
+INSERT INTO Employees (first_name, last_name, role_id)
+VALUES ("Victoria", "Holmes", 5);
+
+INSERT INTO Employees (first_name, last_name, role_id)
+VALUES ("Darlene", "Berger", 3);
+
+INSERT INTO Employees (first_name, last_name, role_id)
+VALUES ("Shelley", "Wright", 8);
+
+
