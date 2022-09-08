9679c1e425245b0c821580f3b34199c7bcbcb3d9
itsDenMat <dennismateo93@gmail.com>
2022-09-07 22:21:03 -0700

    Add unction to view Departments, Roles, and Employees

--- server.js
+++ server.js
@@ -1,16 +1,15 @@
 const inquirer = require("inquirer");
 const mysql = require("mysql");
-// const consoleTable = require("console.table");
+const consoleTable = require("console.table");
 
 var connection = mysql.createConnection({
   host: "localhost",
-  port: 3306,
+  port: 3001,
   user: "root",
   password: "Caden#0207",
   database: "employee_tracker_db"
 });
 
-// update server
 function updateServer() {
   connection.query("SELECT * from role", function(error, res) {
     allroles = res.map(role => ({ name: role.title, value: role.id }));
@@ -87,4 +86,36 @@ function startEmployeeManager() {
           break;
       }
     });
+}
+
+function viewAllDepartments() {
+  console.log("   ");
+  var query = "SELECT id, name AS department FROM department";
+  connection.query(query, function(err, res) {
+    if (err) throw err;
+    console.table(res);
+    startEmployeeManager();
+  });
+}
+
+function viewAllEmployees() {
+  console.log("   ");
+  var query =
+    "SELECT employee.id, first_name AS firstname, last_name AS lastname, title AS role, name AS department, salary as salary FROM employee INNER JOIN role ON employee.role_id = role.id INNER JOIN department ON role.department_id = department.id;";
+  connection.query(query, function(err, res) {
+    if (err) throw err;
+    console.table(res);
+    startEmployeeManager();
+  });
+}
+
+function viewAllRoles() {
+  console.log("   ");
+  var query =
+    "SELECT r.id, title AS role, salary, name AS department FROM role r LEFT JOIN department d ON department_id = d.id";
+  connection.query(query, function(err, res) {
+    if (err) throw err;
+    console.table(res);
+    startEmployeeManager();
+  });
 }
