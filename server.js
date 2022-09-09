919afad9e814b78e34f59337fd32c19c5d365ec8
itsDenMat <dennismateo93@gmail.com>
2022-09-08 22:53:46 -0700

    Update query to view employees in ascending order

--- server.js
+++ server.js
@@ -99,7 +99,7 @@ function viewAllEmployees() {
   console.log("   ");
   // Employee query
   var query =
-    "SELECT employee.first_name, employee.last_name, role.title, role.salary, department.name, CONCAT(e.first_name, ' ' ,e.last_name) AS Manager FROM employee INNER JOIN role on role.id = employee.role_id INNER JOIN department on department.id = role.department_id left join employee e on employee.manager_id = e.id;";
+    "SELECT employee.id, employee.first_name, employee.last_name, role.title, role.salary, department.name, CONCAT(e.first_name, ' ' ,e.last_name) AS Manager FROM employee INNER JOIN role on role.id = employee.role_id INNER JOIN department on department.id = role.department_id left join employee e on employee.manager_id = e.id ORDER BY id ASC;";
   connection.query(query, function(err, res) {
     if (err) throw err;
     console.table(res);
