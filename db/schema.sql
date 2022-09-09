751179eea47b28aa308b315f818a952723aee8cc
itsDenMat <dennismateo93@gmail.com>
2022-09-08 22:35:53 -0700

    Add foreign key in schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -8,26 +8,26 @@ USE employee_tracker_db;
 
 -- Creates the table for list of department types 
 CREATE TABLE department (
-    id INT NOT NULL AUTO_INCREMENT,
-    name VARCHAR(30) NOT NULL,
-    PRIMARY KEY (id)
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30) NOT NULL 
 );
 
 -- Creates the table for list of role types
 CREATE TABLE role (
-    id INT NOT NULL AUTO_INCREMENT,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL (10,2) NOT NULL,
     department_id INT NOT NULL,
-    PRIMARY KEY (id)
+    FOREIGN KEY (department_id) REFERENCES department(id)
 );
 
 -- Creates the table for list of Employees
 CREATE TABLE employee (
-    id INT NOT NULL AUTO_INCREMENT,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INT NOT NULL,
+    FOREIGN KEY (role_id) REFERENCES role(id),
     manager_id INT NULL,
-    PRIMARY KEY (id) 
+    FOREIGN KEY (manager_id) REFERENCES employee(id)
 );
